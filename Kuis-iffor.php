4fd4be9e0b3365951318e5dcb749872a43245f30
Egan Wiryawan <110010560+eganwiryawan16@users.noreply.github.com>
2022-09-01 21:57:21 +0700

    Add files via upload

---
+++ Kuis-iffor.php
@@ -0,0 +1,95 @@
+<?php
+	$star=10;
+	for( $a=$star;$a>0;$a--){
+	for($i=1; $i<=$a; $i++){
+		echo "&nbsp";
+	}
+	for($a1=$star;$a1>=$a;$a1--){
+		echo"*";
+	}
+	echo"<br>";
+	}
+
+    echo"<hr>";
+    $star=10;
+	for($a=$star;$a>0;$a--){
+	for($b=$star;$b>=$a;$b--){
+		echo "*";
+	}
+	echo "<br>";
+	}
+
+    echo"<hr>";
+    $star=10;
+	for($a=$star;$a>0;$a--){
+	for($i=1; $i<=$a; $i++){
+		echo " &nbsp";
+	}
+	for($a1=$star;$a1>=$a;$a1--){
+		echo"*";
+	}
+	echo"<br>";
+	}
+    
+    echo"<hr>";
+    $star=10;
+	for($a=1; $a<=$star; $a++){
+	for($b=1; $b<=$a; $b++){
+		echo "&nbsp";
+	}
+	for($c=$star; $c>=$a; $c-=1){
+		echo "*";
+	}
+	echo "<br>";
+	}
+    
+    echo"<hr>";
+    $star=10;
+	for($a=1; $a<=$star; $a++){
+	for($i=1; $i<=$a; $i++){
+		echo " &nbsp";
+	}
+	for($c=$star; $c>=$a; $c-=1){
+		echo "*";
+	}
+	echo "<br>";
+	}
+
+    echo"<hr>";
+    $star=10;
+	for($a=1; $a<=$star; $a++){
+	for($c=$star; $c>=$a; $c-=1){
+		echo "*";
+	}
+	echo "<br>";
+	}
+
+    echo"<hr>";
+    $star=10;
+	for($a=1; $a<=$star; $a++){
+	for($b=$star; $b>=$a; $b-=1){
+		echo "&nbsp";
+	}
+	for($c=1; $c<=$a; $c++){
+		echo "*";
+	}
+	echo "<br>";
+	}
+	for($a=1; $a<=$star; $a++){
+	for($b=1; $b<=$a; $b++){
+		echo "&nbsp";
+	}
+	for($c=$star; $c>=$a; $c-=1){
+		echo "*";
+	}
+	echo "<br>";
+	}
+
+    echo"<hr>";
+    for ($i=1;$i<=5;$i++){
+        for ($j=$i;$j>=1;$j--){
+            echo $j;
+        }
+        echo "<br>";
+    }
+?>
